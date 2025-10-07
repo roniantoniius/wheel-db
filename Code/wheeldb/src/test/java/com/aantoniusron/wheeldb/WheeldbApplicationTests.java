b64cc5c714efccfa1a9805b19e39742546271ba3
unknown <aantoniusron@gmail.com>
2025-10-07 21:14:23 +0700

    init project maven, before testing

---
+++ Code/wheeldb/src/test/java/com/aantoniusron/wheeldb/WheeldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aantoniusron.wheeldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheeldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
